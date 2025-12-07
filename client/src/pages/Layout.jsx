c2d07387e50a4d3e5e2076551b8238fe698d1d31
Shriti Negi <shritinegi24@gmail.com>
2025-12-07 19:07:36 +0530

    added navbar

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const Layout = () => {
   return (
     <div>
-     <h1>Layout</h1> 
-     <div><Outlet/></div>
+     <div className='min-h-screen bg-gray-50'>
+        <Navbar/>
+        <Outlet/>
+     </div>
     </div>
   )
 }
