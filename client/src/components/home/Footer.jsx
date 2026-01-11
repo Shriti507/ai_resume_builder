16fe87c7464dd395cc3dae5b7c5b568a58d57556
Shriti Negi <shritinegi24@gmail.com>
2026-01-11 12:00:00 +0530

    docs: document social media links in footer

--- client/src/components/home/Footer.jsx
+++ client/src/components/home/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
+    /* Social identity links for the footer */
     const socials = [
         {
             href: '#',
